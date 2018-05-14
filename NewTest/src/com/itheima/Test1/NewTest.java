c8f362ae2d604f57a1d09cf0be8f4155a553af15
handsomeLi <18511024392@163.com>
2018-05-14 21:31:24 +0800

    新新新新

--- NewTest/src/com/itheima/Test1/NewTest.java
+++ NewTest/src/com/itheima/Test1/NewTest.java
@@ -2,7 +2,8 @@ package com.itheima.Test1;
 
 public class NewTest {
 	public static void main(String[] args) {
-		System.out.println("修改了");
+		System.out.println("修改了1");
+		System.out.println("进行了新的一轮调试");
 	}
 
 }
